ef785e7018e09f6cc929ab437efedde8ea25d068
Roger Samsó <rogersamso@gmail.com>
2022-08-17 10:21:21 +0200

    deploy docs

--- docs/make.jl
+++ docs/make.jl
@@ -4,9 +4,21 @@ DocMeta.setdocmeta!(IdealistaAPIClient, :DocTestSetup, :(using IdealistaAPIClien
 
 makedocs(sitename="IdealistaAPIClient.jl",
          modules = [IdealistaAPIClient],
-        pages = [
-        "Introduction" => "index.md",
-        "Usage" => "usage.md",
-        "API" => Any["Types" => "types.md",
-                     "Functions" => "functions.md"]
-    ])
+         authors="Roger Samsó <r.samso@proton.me>",
+         format=Documenter.HTML(
+                                prettyurls=get(ENV, "CI", nothing) == "true",
+                                canonical="https://rogersamso.github.io/IdealistaAPIClient.jl",
+                                assets=String[],
+                               ),
+         pages = [
+            "Introduction" => "index.md",
+            "Usage" => "usage.md",
+             "API" => Any["Types" => "types.md",
+                          "Functions" => "functions.md"]
+            ],
+        )
+
+deploydocs(
+           repo="github.com/rogersamso/IdealistaAPIClient.jl.git",
+           devbranch="main",
+          )
